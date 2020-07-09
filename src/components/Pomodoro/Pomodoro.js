bc7d9d18ea5764c839220490b78c9252a088c9af
Dextavision <dextavision@gmail.com>
2020-07-09 21:13:03 +0200

    added timer input so the user can decide on the pomodoro time

--- src/components/Pomodoro/Pomodoro.js
+++ src/components/Pomodoro/Pomodoro.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Pomodoro.scss";
 import Button from "../Button/Button";
 import Progressbar from "../Progressbar/Progressbar";
+import Timer from "../Timer/Timer";
 
 function Pomodoro() {
   return (
     <div className='pomodoro'>
       <h2 className='pomo-header'>Pomodoro Section</h2>
       <Button />
+      <Timer />
       <Progressbar />
     </div>
   );
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Timer.scss";
 
 function Timer() {
   return (
-    <div>
-      <div></div>
+    <div className='timer'>
+      <h1>Time</h1>
+      <input type='number'></input>
     </div>
   );
 }
