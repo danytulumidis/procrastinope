96f680a8f0941bd2629d915a9cd00b8ca2d9ed81
Dextavision <dextavision@gmail.com>
2020-10-25 16:14:39 +0100

    Updates

---
+++ precache-manifest.c50ec55aadee350101588cf6b2deec6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf060aa9673b8edd219e4ee812bc51d",
+    "url": "/procrastinope/index.html"
+  },
+  {
+    "revision": "065d4f5589a0b1fa359e",
+    "url": "/procrastinope/static/css/main.13dbc668.chunk.css"
+  },
+  {
+    "revision": "7b9c66376f7b5aba6ad4",
+    "url": "/procrastinope/static/js/2.1523f99b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/procrastinope/static/js/2.1523f99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065d4f5589a0b1fa359e",
+    "url": "/procrastinope/static/js/main.41774780.chunk.js"
+  },
+  {
+    "revision": "6d9bbb0de5d95762aa1c",
+    "url": "/procrastinope/static/js/runtime-main.92401d14.js"
+  }
+]);
