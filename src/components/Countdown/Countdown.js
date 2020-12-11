22e2dabe441c6ec845d9c859bf4b9f53a891ee62
dtulumidis <dextavision@gmail.com>
2020-12-11 16:45:53 +0100

    new component for countdown

---
+++ src/components/Countdown/Countdown.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Countdown.scss";
+
+const Countdown = () => {
+  return (
+    <div>
+      <h1>Countdown</h1>
+    </div>
+  );
+};
+
+export default Countdown;
